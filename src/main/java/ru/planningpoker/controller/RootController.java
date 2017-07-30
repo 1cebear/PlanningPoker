c36675460ab10b8f93a7d8ca0578d00c0072addb
1cebear <ghost1515@yandex.ru>
2017-07-30 19:23:28 +0300

    jsp controller

---
+++ src/main/java/ru/planningpoker/controller/RootController.java
@@ -0,0 +1,33 @@
+package ru.planningpoker.controller;
+
+/**
+ * Created by Icebear on 06.06.2017.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class RootController {
+
+    @GetMapping(value = "/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping(value = "/index")
+    public String login() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String users(Model model) {
+        return "main";
+    }
+
+}
+
